6d374596b352687d1f93df4aafbe554ecbc0a492
Saketh2611 <ae22b061@smail.iitm.ac.in>
2026-03-30 02:49:02 +0530

    added docker and image logo

--- backend/app.js
+++ backend/app.js
@@ -37,7 +37,7 @@ app.use((err, req, res, next) => {
   res.status(500).send('Something broke!');
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = 5000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
